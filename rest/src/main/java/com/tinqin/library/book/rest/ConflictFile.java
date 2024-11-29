0698072ebe33de544e005f129485d000e55a443f
Plamen Simeonov <haibusa1991@gmail.com>
2024-11-29 10:51:37 +0200

    delete conflict file

--- rest/src/main/java/com/tinqin/library/book/rest/ConflictFile.java
+++
@@ -1,8 +0,0 @@
-package com.tinqin.library.book.rest;
-
-public class ConflictFile {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("Hello, Java!");
-    }
-}
