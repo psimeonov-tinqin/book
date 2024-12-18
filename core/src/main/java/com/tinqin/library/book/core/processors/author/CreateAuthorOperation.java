400cff3cb41a0c751652b994e99beaeba5b94273
k.ivanova <k.ivanova@tinqin.com>
2024-12-18 11:31:28 +0200

    create Kafka

--- core/src/main/java/com/tinqin/library/book/core/processors/author/CreateAuthorOperation.java
+++ core/src/main/java/com/tinqin/library/book/core/processors/author/CreateAuthorOperation.java
@@ -5,9 +5,9 @@ import com.tinqin.library.book.api.author.create.CreateAuthorInput;
 import com.tinqin.library.book.api.author.create.CreateAuthorOutput;
 import com.tinqin.library.book.api.errors.OperationError;
 import com.tinqin.library.book.core.errorhandler.base.ErrorHandler;
-import com.tinqin.library.book.domain.ReportingClient;
 import com.tinqin.library.book.persistence.models.Author;
 import com.tinqin.library.book.persistence.repositories.AuthorRepository;
+import com.tinqin.library.reporting.KafkaProducerService;
 import io.vavr.control.Either;
 import io.vavr.control.Try;
 import lombok.RequiredArgsConstructor;
@@ -17,41 +17,38 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class CreateAuthorOperation implements CreateAuthor {
-    private final AuthorRepository authorRepository;
-    private final ConversionService conversionService;
-    private final ErrorHandler errorHandler;
-    private final ReportingClient reportingClient;
+
+  private final AuthorRepository authorRepository;
+  private final ConversionService conversionService;
+  private final ErrorHandler errorHandler;
+private final KafkaProducerService  producerService;
 
 
-    @Override
-    public Either<OperationError, CreateAuthorOutput> process(CreateAuthorInput input) {
-        return createRecord(input)
-                .flatMap(this::persistAuthor)
-                .toEither()
-                .mapLeft(errorHandler::handle);
-    }
 
-    private Try<CreateAuthorInput> createRecord(CreateAuthorInput input) {
-        return Try.of(() -> {
-            reportingClient.createRecord();
-            return input;
-        });
+  @Override
+  public Either<OperationError, CreateAuthorOutput> process(CreateAuthorInput input) {
+    return
+        persistAuthor(input)
+            .toEither()
+            .mapLeft(errorHandler::handle);
+  }
 
-    }
 
-    private Try<CreateAuthorOutput> persistAuthor(CreateAuthorInput input) {
-        return Try.of(() -> {
+  private Try<CreateAuthorOutput> persistAuthor(CreateAuthorInput input) {
+    return Try.of(() -> {
 
-            Author author = conversionService.convert(input, Author.class);
+      Author author = conversionService.convert(input, Author.class);
 
-            Author persisted = authorRepository.save(author);
+      Author persisted = authorRepository.save(author);
 
-            return CreateAuthorOutput
-                    .builder()
-                    .authorId(persisted.getId())
-                    .build();
-        });
-    }
+      producerService.createAuthorRecord(persisted.getId());
+
+      return CreateAuthorOutput
+          .builder()
+          .authorId(persisted.getId())
+          .build();
+    });
+  }
 
 
 }
